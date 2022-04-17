338cae9caf5b55659192a8a12294c15b0f014eea
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-04-17 18:06:21 +0900

    ex03

---
+++ ex03/createStairs.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	max, _ := strconv.Atoi(os.Args[1])
+	cnt := 0
+	row := 1
+	for {
+		if cnt + row > max {
+			break
+		}
+		for i := 0; i < row; i++ {
+			fmt.Printf("*")
+			cnt++
+		}
+		fmt.Print("\n")
+		row++
+	}
+}
